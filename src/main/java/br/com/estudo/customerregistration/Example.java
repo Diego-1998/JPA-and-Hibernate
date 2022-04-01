fc7fad21b1490f1f1afd781e93efc6794e81551c
Diego leandro <diegolgaspar000@gmail.com>
2022-04-01 14:10:22 -0300

    project update

--- src/main/java/br/com/estudo/customerregistration/Example.java
+++ src/main/java/br/com/estudo/customerregistration/Example.java
@@ -13,8 +13,18 @@ public class Example {
                 .createEntityManagerFactory("Client-PU");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Client cliente = entityManager.find(Client.class, 1);
-        System.out.println(cliente.getNome());
+       // Client cliente = entityManager.find(Client.class, 1);
+       //System.out.println(cliente.getNome());
+
+        Client client = new Client();
+        //client.setId(2);
+        client.setNome("Supermarket");
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(client);
+
+        entityManager.getTransaction().commit();
 
         entityManager.close();
         entityManagerFactory.close();
--- src/main/java/br/com/estudo/customerregistration/model/Client.java
+++ src/main/java/br/com/estudo/customerregistration/model/Client.java
@@ -1,6 +1,8 @@
 package br.com.estudo.customerregistration.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
@@ -8,6 +10,7 @@ import java.util.Objects;
 public class Client {
 
     @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
