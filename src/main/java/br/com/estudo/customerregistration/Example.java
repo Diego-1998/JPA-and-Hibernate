08fd8b466151f2165ebefbc93fd632b089996ea0
Diego leandro <diegolgaspar000@gmail.com>
2022-04-02 13:04:09 -0300

    project update1

--- src/main/java/br/com/estudo/customerregistration/Example.java
+++ src/main/java/br/com/estudo/customerregistration/Example.java
@@ -12,19 +12,35 @@ public class Example {
         EntityManagerFactory entityManagerFactory = Persistence
                 .createEntityManagerFactory("Client-PU");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+/*
 
-       // Client cliente = entityManager.find(Client.class, 1);
-       //System.out.println(cliente.getNome());
+        Client cliente = entityManager.find(Client.class, 1);
+        System.out.println(cliente.getNome());
 
         Client client = new Client();
-        //client.setId(2);
-        client.setNome("Supermarket");
+        client.setNome("Casas Bahia");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(client);
+        entityManager.getTransaction().commit();
+
+        Client client = entityManager.find(Client.class, 2);
+
+        entityManager.getTransaction().begin();
+        entityManager.remove(client);
+        entityManager.getTransaction().commit();
+*/
+
+        Client client = new Client();
+        client.setId(1);
+        client.setNome("Mercado Livre");
 
         entityManager.getTransaction().begin();
 
-        entityManager.persist(client);
+        entityManager.merge(client);
 
         entityManager.getTransaction().commit();
+
 
         entityManager.close();
         entityManagerFactory.close();
